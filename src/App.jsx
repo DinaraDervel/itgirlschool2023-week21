8f0390a9db538026b344e1626f9309bf40b23b31
DinaraDervel <dinaradervel@gmail.com>
2023-09-13 14:45:41 -0600

    add chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Chart } from "react-google-charts";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const data = [
+    ["Language", "Percent"],
+    ["JavaScript", 57.85],
+    ["TypeScript", 50.3],
+    ["C#", 10.63],
+    ["Dart", 4.96],
+    ["Другой", 9.45],
+  ];
 
+  const options = {
+    title: "На каком языке вы программируете? (Front-End)",
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width={"100%"}
+        height={"400px"}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
